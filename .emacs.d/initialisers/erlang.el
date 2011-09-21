1c2b807b340dab8efb9a83e7ad4bc9701decab0a
Brendan Hay <brendan.g.hay@gmail.com>
2011-09-21 10:27:50 +0200

    Removing packages

--- .emacs.d/initialisers/erlang.el
+++
@@ -1,5 +0,0 @@
-(require 'erlang)
-
-(setq auto-mode-alist 
-      (cons '("\\.hrl$" . erlang-mode) (cons '("\\.erl$" . erlang-mode) auto-mode-alist)))
-
--- .emacs.d/initialisers/tramp.el
+++
@@ -1,7 +0,0 @@
-(require 'tramp)
-
-(setq tramp-default-method "scp")
-(setq tramp-auto-save-directory temp-dir)
-(setq tramp-backup-directory-alist backup-directory-alist)
-(setq tramp-default-method "plink")
-(setq auto-save-file-name-transforms nil)
