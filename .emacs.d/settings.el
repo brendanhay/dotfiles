9fa37bf462209c0ca0736c151aa88285a25c2008
Brendan Hay <brendan.g.hay@gmail.com>
2011-09-21 16:59:19 +0200

    Updating faces and tmux conf

--- .emacs.d/settings.el
+++ .emacs.d/settings.el
@@ -1,16 +1,8 @@
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(custom-safe-themes (quote ("fa4a96fbf85d8ab62b906268f5ad3e37938c2810" "9cdf9fb94f560902b567b73f65c2ed4e5cfbaafe" default))))
 
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+ '(vertical-border ((nil (:foreground "grey20")))))
 
 (load-theme 'zenburn)
 
