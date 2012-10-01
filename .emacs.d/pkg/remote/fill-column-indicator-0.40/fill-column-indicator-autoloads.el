64a5e09a65e7455b6e56767efcb532abac229723
Brendan Hay <brendan.g.hay@gmail.com>
2012-10-01 09:35:56 +0200

    Removing old style links

--- .emacs.d/pkg/remote/fill-column-indicator-0.40/fill-column-indicator-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; fill-column-indicator-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("fill-column-indicator-pkg.el" "fill-column-indicator.el")
-;;;;;;  (20108 8263 107666))
-
-;;;***
-
-(provide 'fill-column-indicator-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; fill-column-indicator-autoloads.el ends here
