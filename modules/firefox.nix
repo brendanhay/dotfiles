fd97a010bc236cdd036a59df56d099d87ac6fc72
Henrik Lissner <git@henrik.io>
2023-07-08 13:03:09 +1200

    WIP

--- modules/firefox.nix
+++ modules/firefox.nix
@@ -1,3 +1,28 @@
-{ ... }:
+{ config, pkgs, lib, ... }:
 
-{ }
+with lib;
+
+let
+  cfg = config.modules.firefox;
+in
+{
+  options.modules.firefox = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+    };
+  };
+
+  config = mkIf cfg.enable {
+    modules.home-manager.programs.firefox = {
+      enable = true;
+      profiles.${config.modules.user.name} = {
+        extensions = with inputs.nur.repos.rycee.firefox-addons; [
+          bitwarden
+          ublock-origin
+          privacy-badger
+        ];
+      };
+    };
+  };
+}
