99f52a781321a27c699034b7967e397c3d423b9b
Brendan Hay <os@brendanhay.nz>
2023-07-09 15:50:32 +1200

    WIP

---
+++ modules/alacritty.nix
@@ -0,0 +1,19 @@
+{ config, ... }:
+
+with lib;
+
+let
+  cfg = config.modules.alacritty;
+in
+{
+  options.modules.alacritty = {
+    enable = mkOption {
+      type = types.bool
+        default = false;
+    };
+  };
+
+  config = {
+    home-manager.programs.alacritty.enable = true;
+  };
+}
